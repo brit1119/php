206ebb934fe5a22dbf8c66ac3aa4bb2dfb8b5927
Britney Tang <britneytang03@gmail.com>
2023-03-03 16:04:17 +0800

    add total php

---
+++ total.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+
+
+    <?php
+    $sum = 0;
+    $x = 1;
+
+    while ($x <= 100) {
+        $sum += $x;
+        $x++;
+    }
+
+    echo "$sum";
+    ?>
+
+
+</body>
+
+</html>
