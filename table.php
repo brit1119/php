e189c9ed3fbc400b9e110633bcad503556478a63
Britney Tang <britneytang03@gmail.com>
2023-03-03 16:36:56 +0800

    add table php

---
+++ table.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <table align="left" border="1" cellpadding="15" cellspacing="0" width="300">
+
+        <?php
+        $x = 1;
+
+        while ($x <= 100) {
+            echo "<tr> <td> </td> </tr>";
+        }
+        $x++;
+        ?>
+    </table>
+</body>
+
+</html>
