89f81aeb3925ab8e95d57d40a71531dd76b68f2b
Britney Tang <britneytang03@gmail.com>
2023-03-04 13:15:35 +0800

    add table-alternate

---
+++ table-alternate.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <table align="left" border="1" cellpadding="15" cellspacing="0" width="700">
+
+        <?php
+        $x = 1;
+        while ($x <= 100) {
+            $x++;
+            if ($x % 2 == 0) {
+                echo "<tr> <td></td> <td></td> <td></td> </tr>";
+            } else {
+                echo '<tr style="background-color: #ECECEC;"> <td></td> <td></td> <td></td></tr>';
+            }
+        }
+        ?>
+
+    </table>
+
+</body>
+
+</html>
