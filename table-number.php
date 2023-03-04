4909a89fa70aee78aeca5a3b9bbfc9daafa161d6
Britney Tang <britneytang03@gmail.com>
2023-03-04 21:16:25 +0800

    add table number php

---
+++ table-number.php
@@ -0,0 +1,18 @@
+<html>
+
+<body>
+
+    <table>
+        <?php
+        $x = 1;
+        while ($x <= 100) {
+            echo  "row  $x  -  $x ";
+            echo '<br>';
+            $x++;
+        }
+        ?>
+    </table>
+
+</body>
+
+</html>
